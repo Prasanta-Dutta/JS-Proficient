4272d9a4295b32047f95254d8951a760da9b5789
Prasanta Dutta <82324631+Prasanta-Dutta@users.noreply.github.com>
2025-04-13 20:12:52 +0000

    Concept on comparison

---
+++ comparison.js
@@ -0,0 +1,37 @@
+/*
+console.log(null > 0)   //  false
+console.log(null < 0)   //  false
+
+console.log(null == 0)  //  false
+
+console.log(null >= 0)  //  true
+console.log(null <= 0)  //  true
+*/
+
+
+/*
+console.log(undefined > 0)   //  false
+console.log(undefined < 0)   //  false
+
+console.log(undefined == 0)  //  false
+
+console.log(undefined >= 0)  //  false
+console.log(undefined <= 0)  //  false
+*/
+
+
+/*
+console.log(NaN > 0)   //  false
+console.log(NaN < 0)   //  false
+
+console.log(NaN == 0)  //  false
+
+console.log(NaN >= 0)  //  false
+console.log(NaN <= 0)  //  false
+*/
+
+
+console.log("" == "");      //  true
+console.log("" == " ");     //  false
+console.log("" == null);    //  false
+console.log(2 == "2");      //  true
